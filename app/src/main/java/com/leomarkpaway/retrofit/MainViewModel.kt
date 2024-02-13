8d547464d70c786a21f46aab11122628880dfa5d
leomark paway <leomarkpaway@gmail.com>
2024-02-13 14:56:26 +0800

    :sparkles: Implement api client on view model

--- app/src/main/java/com/leomarkpaway/retrofit/MainViewModel.kt
+++ app/src/main/java/com/leomarkpaway/retrofit/MainViewModel.kt
@@ -1,6 +1,40 @@
 package com.leomarkpaway.retrofit
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.leomarkpaway.retrofit.api.ApiClient
+import com.leomarkpaway.retrofit.model.AllPost
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel: ViewModel() {
+    private val apiClient by lazy { ApiClient.apiService }
+
+    private val _allPost = MutableLiveData<AllPost>()
+    val allPost: LiveData<AllPost> = _allPost
+
+    fun getAllPost() {
+        val allPostCallBack = apiClient.getAllPost()
+
+        allPostCallBack.enqueue(object : Callback<AllPost> {
+            override fun onResponse(call: Call<AllPost>, response: Response<AllPost>) {
+                if (response.isSuccessful) {
+                    val response = response.body()
+                    _allPost.value = response
+                } else {
+                    // Handle error
+                    Log.d("qwe", "error")
+                }
+            }
+
+            override fun onFailure(call: Call<AllPost>, t: Throwable) {
+                // Handle failure
+                Log.d("qwe", "onFailure $call - $t")
+            }
+        })
+
+    }
 }
