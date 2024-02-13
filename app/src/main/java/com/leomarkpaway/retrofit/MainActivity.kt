f8738ea6fd6d000fb56fc2a4e3d155701ef9d4fc
leomark paway <leomarkpaway@gmail.com>
2024-02-13 13:26:52 +0800

    :art: implement base activity

--- app/src/main/java/com/leomarkpaway/retrofit/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/retrofit/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.leomarkpaway.retrofit
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.activity.viewModels
+import com.leomarkpaway.android_basics.common.base.BaseActivity
+import com.leomarkpaway.retrofit.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
+    override val viewModel: MainViewModel by viewModels()
+    override fun inflateBinding() = ActivityMainBinding.inflate(layoutInflater)
+
+    override fun setupViews() {
+        TODO("Not yet implemented")
+    }
+
+    override fun subscribe() {
+        TODO("Not yet implemented")
     }
 }
